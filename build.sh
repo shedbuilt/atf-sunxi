f0c7bd9cdb8563bf02ef43ea891fad3cf44cf3b7
Auston Stewart <auston@shedbuilt.net>
2018-07-03 08:05:36 -1000

    Updating for package options in blank

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-case "$SHED_DEVICE" in
-    orangepi-pc2)
-        SHDPKG_ATF_PLATFORM='sun50iw1p1'
-        SHDPKG_ATF_TARGET='bl31'
-        SHDPKG_ATF_PRODUCT='build/sun50iw1p1/debug/bl31.bin'
-        ;;
-    *)
-        echo "Unsupported device: $SHED_DEVICE"
-        exit 1
-        ;;
-esac
-make PLAT="$SHDPKG_ATF_PLATFORM" DEBUG=1 "$SHDPKG_ATF_TARGET" &&
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+for SHED_PKG_LOCAL_OPTION in "${!SHED_PKG_LOCAL_OPTIONS[@]}"; do
+    # Configure
+    case "$SHED_PKG_LOCAL_OPTION" in
+        nanopineo2|nanopineoplus2|orangepipc2|nanopik1plus)
+            SHED_PKG_ATF_PLATFORM='sun50iw1p1'
+            SHED_PKG_ATF_TARGET='bl31'
+            SHED_PKG_ATF_PRODUCT='build/sun50iw1p1/debug/bl31.bin'
+            ;;
+    esac
+done
+make PLAT="$SHED_PKG_ATF_PLATFORM" DEBUG=1 "$SHED_PKG_ATF_TARGET" &&
 install -dm755 "${SHED_FAKE_ROOT}/boot/u-boot" &&
-install -m644 "$SHDPKG_ATF_PRODUCT" "${SHED_FAKE_ROOT}/boot/u-boot"
+install -m644 "$SHED_PKG_ATF_PRODUCT" "${SHED_FAKE_ROOT}/boot/u-boot"
