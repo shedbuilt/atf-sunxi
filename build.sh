da4372602c687f2285ffefbe5d824b5ee65a669d
Auston Stewart <auston@shedbuilt.net>
2018-10-25 09:22:51 -1000

    Updating to use build option aliases

--- build.sh
+++ build.sh
@@ -3,13 +3,14 @@ declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
 for SHED_PKG_LOCAL_OPTION in "${!SHED_PKG_LOCAL_OPTIONS[@]}"; do
     # Configure
     case "$SHED_PKG_LOCAL_OPTION" in
-        nanopineo2|nanopineoplus2|orangepipc2|nanopik1plus)
+        sun50iw1p1)
             SHED_PKG_ATF_PLATFORM='sun50iw1p1'
             SHED_PKG_ATF_TARGET='bl31'
             SHED_PKG_ATF_PRODUCT='build/sun50iw1p1/debug/bl31.bin'
             ;;
     esac
 done
+# Build and Install
 make PLAT="$SHED_PKG_ATF_PLATFORM" DEBUG=1 "$SHED_PKG_ATF_TARGET" &&
 install -dm755 "${SHED_FAKE_ROOT}/boot/u-boot" &&
 install -m644 "$SHED_PKG_ATF_PRODUCT" "${SHED_FAKE_ROOT}/boot/u-boot"
